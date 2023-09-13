3ffe8a6d5cc24ba009ed2502f10f45f99a9db271
9aiquant <aiquant9@gmail.com>
2023-09-13 21:45:14 +0800

    x

--- main.py
+++ main.py
@@ -1,16 +1,38 @@
-# This is a sample Python script.
+from sanic import Sanic, json, SanicException, text
+from sanic.log import logger
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import akshare
+
+app = Sanic(name='quant-api')
+
+date_format = '%Y-%m-%d %H:%M:%S'
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+@app.get("/")
+async def hello_world(request):
+    return text("Hello, world.")
 
 
-# Press the green button in the gutter to run the script.
+@app.get("rest")
+def rest(request):
+    cmd = request.args.get("cmd")
+    logger.info('cmd is %s' % cmd)
+    # import Ashare
+    try:
+        df = eval(str(cmd))
+    except Exception as e:
+        logger.error("eval is running at error:%s" % e)
+        raise SanicException("Something went wrong.%s" % (e), status_code=501)
+    logger.info('df len is %d' % len(df))
+
+    # default is a function applied to objects that aren't serializable.
+    # In this case it's str, so it just converts everything it doesn't know to strings.
+    return json(df.dropna().to_dict('records'), headers={"charset": "utf-8"},
+                default=str,
+                ensure_ascii=False)
+
+    # return JsonResponse(json.loads(response), json_dumps_params={'ensure_ascii': False}, safe=False)
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app.run(host="0.0.0.0", port=80)
